381751fed552b228600882e216c726ba1d4ea0a6
Waniza Khan <160387185+winikhan@users.noreply.github.com>
2024-10-29 19:07:43 +0500

    Add files via upload

---
+++ file.ts
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggle-button') as HTMLButtonElement
+const skills = document.getElementById('skills') as HTMLElement
+
+toggleButton.addEventListener('click'  , ()=>{
+    if(skills.style.display === 'none') {
+        skills.style.display ='block'
+    } else {
+        skills.style.display = 'none'
+    }
+});
